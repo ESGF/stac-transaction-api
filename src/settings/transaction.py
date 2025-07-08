b4dfe905f8fee7453c04d8309cbb7df0caf265f7
rhysrevans3 <rhys.r.evans3@gmail.com>
2025-07-08 13:15:45 +0100

    Updating regexes.

--- src/settings/transaction.py
+++ src/settings/transaction.py
@@ -75,6 +75,38 @@ else:
 default_extensions = {
     "CMIP6": {
         "CMIP6": {
+            "regex": [r"https:\/\/stac-extensions\.github\.io\/cmip6\/v[0-9]\.[0-9]\.[0-9]/schema\.json"],
+            "default": "https://stac-extensions.github.io/cmip6/v1.0.0/schema.json",
+        },
+        "alternate_assets": {
+            "regex": [r"https:\/\/stac-extensions\.github\.io\/alternate-assets\/v[0-9]\.[0-9]\.[0-9]\/schema\.json"],
+            "default": "https://stac-extensions.github.io/alternate-assets/v1.2.0/schema.json",
+        },
+        "file": {
+            "regex": [r"https:\/\/stac-extensions\.github\.io\/file\/v[0-9]\.[0-9]\.[0-9]/schema\.json"],
+            "default": "https://stac-extensions.github.io/file/v2.1.0/schema.json",
+        },
+    },
+    "CMIP7": {
+        "CMIP7": {
+            "regex": [r"https:\/\/stac-extensions\.github\.io\/cmip7\/v[0-9]\.[0-9]\.[0-9]\/schema\.json"],
+            "default": "https://stac-extensions.github.io/cmip7/v1.0.0/schema.json",
+        },
+        "alternate_assets": {
+            "regex": [r"https:\/\/stac-extensions\.github\.io\/alternate-assets\/v[0-9]\.[0-9]\.[0-9]\/schema\.json"],
+            "default": "https://stac-extensions.github.io/alternate-assets/v1.2.0/schema.json",
+        },
+        "file": {
+            "regex": [r"https:\/\/stac-extensions\.github\.io\/file\/v[0-9]\.[0-9]\.[0-9]/schema\.json"],
+            "default": "https://stac-extensions.github.io/file/v2.1.0/schema.json",
+        },
+    },
+}
+
+
+{
+    "CMIP6": {
+        "CMIP6": {
             "regex": ["https://stac-extensions\.github\.io/cmip6/v[0-9]\.[0-9]\.[0-9]/schema\.json"],
             "default": "https://stac-extensions.github.io/cmip6/v1.0.0/schema.json",
         },
