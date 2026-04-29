6929f5c943a840f860af13bbb37ac9d02f43ab7e
rhysrevans3 <rhys.r.evans3@gmail.com>
2026-04-29 14:10:00 +0100

    authorizer egi not ceda.

--- src/authorizer/__init__.py
+++ src/authorizer/__init__.py
@@ -1,6 +1,6 @@
 from src.settings import settings
 
-if settings.authorizer == "ceda":
+if settings.authorizer == "egi":
     from src.authorizer.egi_authorizer import EGIAuthorizer as Auth
 else:
     from src.authorizer.globus_authorizer import GlobusAuthorizer as Auth
