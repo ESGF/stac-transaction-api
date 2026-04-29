d21fbf1082d23b0755b4c6c8798ff752426ed8cf
rhysrevans3 <rhys.r.evans3@gmail.com>
2026-04-29 13:37:29 +0100

    Move authorizer to own directory.

--- src/authorizer/globus_authorizer.py
+++ src/authorizer/globus_authorizer.py
@@ -1,9 +1,6 @@
 import json
 import logging
 
-import httpx
-from esgf_core_utils.models.auth.egi import EGIAuth
-from esgf_core_utils.models.kafka.events import RequesterData
 from fastapi import Request
 from globus_sdk import AccessTokenAuthorizer, GroupsClient
 from globus_sdk.scopes import GroupsScopes
@@ -134,55 +131,3 @@ class GlobusAuthorizer(BaseHTTPMiddleware):
                     auth_response["context"]["groups"] = json.dumps(groups)
 
         return auth_response
-
-
-class EGIAuthorizer(BaseHTTPMiddleware):
-    """
-    EGI Authorization middleware.
-    """
-
-    async def dispatch(self, request: Request, call_next):
-        # Need to bypass authorization for this endpoint
-        if request.url.path == "/healthcheck":
-            return await call_next(request)
-
-        logger.info("Request Headers %s", request.headers)
-
-        auth = httpx.BasicAuth(
-            username=settings.client.client_id,
-            password=settings.client.client_secret,
-        )
-
-        async with httpx.AsyncClient(timeout=5.0, verify=False) as client:
-            logger.info(
-                "Post request to %s",
-                settings.client.introspection_endpoint,
-            )
-            response = await client.post(
-                settings.client.introspection_endpoint,
-                headers={"Content-type": "application/x-www-form-urlencoded"},
-                data=f"token={request.headers.get('authorization')[7:]}",
-                auth=auth,
-                timeout=5,
-            )
-            response.raise_for_status()
-
-        token_info = response.json()
-
-        authorizer = EGIAuth(
-            regex=settings.client.regex,
-            requester_data=RequesterData(
-                client_id=token_info["client_id"],
-                sub=token_info["sub"],
-                iss=token_info["iss"],
-            ),
-        )
-
-        authorizer.add(token_info["entitlements"])
-
-        request.state.authorizer = authorizer
-
-        return await call_next(request)
-
-
-Authorizer = GlobusAuthorizer if settings.authorizer == "globus" else EGIAuthorizer
