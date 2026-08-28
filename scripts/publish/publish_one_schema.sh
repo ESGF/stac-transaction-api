45b34231fc32057a81b233cbd1bbd0be757350fa
ames4 <ames4@llnl.gov>
2026-08-28 09:07:57 -0700

    update and add

--- scripts/publish/publish_one_schema.sh
+++ scripts/publish/publish_one_schema.sh
@@ -1,26 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 proj=$1
 esgvoc use "$proj@latest"
-ver=`python scripts/publish/get_schema_version.py $proj`
-echo $ver
+ver=$(python scripts/publish/get_schema_version.py "$proj")
+echo "$proj -> $ver"
 
 branch=$(git branch --show-current)
 git checkout gh-pages
-pushd $proj
-esgvoc schema $proj -o schema.json
 
-if [ ! -d $ver ] ; then
-    mkdir $ver
-    mv schema.json $ver
-    git add $ver
-    git commit -m"updating $proj to $ver" 
+mkdir -p "$proj"
+pushd "$proj"
+
+esgvoc schema "$proj" -o schema.json
+
+if [ ! -d "$ver" ]; then
+    mkdir "$ver"
+    mv schema.json "$ver"
+    git add "$ver"
+    git commit -m "updating $proj to $ver"
 else
-    res=`diff schema.json | wc -l`
-    if [ $res ] ; then
-	echo  "Schemas differ for $project/$ver"
+    if diff -q schema.json "$ver/schema.json" > /dev/null; then
+        echo "Matching schemas $proj/$ver"
+        rm schema.json
     else
-       echo  "Matching schemas $project/$ver"
+        echo "Schemas differ for $proj/$ver"
     fi
 fi
-popd
 
-git checkout $branch
+popd
+git checkout "$branch"
